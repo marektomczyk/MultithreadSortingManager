887842b93a60b6a3bcab666d6e50a49c9218f1e3
marek.tomczyk <marek.tomczyk@elzab.pl>
2019-11-21 13:59:37 +0100

    Changes in ToolSet

--- src/tools/ToolSet.cpp
+++ src/tools/ToolSet.cpp
@@ -22,38 +22,123 @@
  *
  *	@return None
  ****************************************************************************/
-void ToolSet::WriteChunkIntoFile(const std::vector<int>& sortedChunk,
-	const unsigned int chunkCount)
+bool ToolSet::WriteChunkIntoFile(
+	const std::vector<int>& sortedChunk, const unsigned int chunkCount)
 {
 	namespace fs = std::filesystem;
+	bool result = false;
 
-	if (sortedChunk.empty())
-		return;
+	if ( sortedChunk.empty() )
+		return false;
 
 	std::string path = "tmp/" + std::to_string(chunkCount) + ".txt";
+	std::ofstream outFile(path);
 
-	std::ofstream outFile;
-	outFile.open(path);
-
-	if (outFile.is_open())
+	if ( outFile.is_open() )
 	{
 		const unsigned int numInRow = 10;
 		unsigned int numCounter = numInRow;
 
-		for (auto num : sortedChunk)
+		for ( auto num : sortedChunk )
 		{
 			outFile << num << " ";
 
 			--numCounter;
-			if (numCounter == 0)
+			if ( numCounter == 0 )
 			{
 				numCounter = numInRow;
 				outFile << "\n";
 			}
 		}
 
+		result = true;
+#if 0
 		LOG_TRACE("Chunk {0} saved successfully", chunkCount);
+#endif
 		outFile.close();
+	}
+
+	return result;
+}
+
+/*****************************************************************************
+ *	@brief  Reads data chunk from file
+ * 
+ *	@note   Reads more data if the position is in the middle of a number
+ *
+ *	@param  file  - file with data
+ *  @param  chunk - vector into which the data will be loaded
+ *  @param  chunkSize - chunk size
+ *  @param  delim - delimeter (by default 0x32 - space)
+ *
+ *	@return true  - when successfully created
+ *	        false - otherwise
+ ****************************************************************************/
+bool ToolSet::ReadChunkFromFile(
+	std::ifstream& file, std::vector<std::byte>& chunk, 
+	std::size_t chunkSize, std::byte delim)
+{
+	bool result = true;
+	if ( !file.is_open() )
+		result = false;
+
+	if ( chunk.capacity() != chunkSize )
+		chunk.resize(chunkSize);
+
+	if ( result )
+		file.read((char*) chunk.data(), chunkSize);
+
+	if ( chunk.empty() )
+		result = false;
+	
+	if ( result )
+	{
+		if ( ( chunk.back() != delim ) && ( !file.eof() ) )
+		{
+			std::byte byte;
+			do
+			{
+				file.read((char*) &byte, sizeof(std::byte));
+				if ( file )
+					chunk.push_back(byte);
+			} while ( ( byte != delim ) || ( !file ) );
+		}
+	}
+	return result;
+}
+
+/*****************************************************************************
+ *	@brief Split data by given delim and convetrs from bytes to integers
+ *
+ *	@param  bytes - vector from data will be converted
+ *  @param  numbers - vector into which the data will be loaded
+ *  @param  delim - delimeter (by default 0x32 - space)
+ *
+ *	@return true  - when successfully created
+ *	        false - otherwise
+ ****************************************************************************/
+void ToolSet::SplitAndConvert(
+	std::vector<std::byte>& bytes, std::vector<int>& numbers,
+	const std::byte delim)
+{
+	if ( !bytes.empty() )
+	{
+		auto iterator = bytes.begin();
+		do
+		{
+			if ( ( iterator != bytes.end() ) && ( *iterator != delim ) )
+			{
+				numbers.push_back((int) *iterator);
+				bytes.erase(iterator);
+			}
+
+			iterator = std::find_if(iterator, bytes.end(),
+				[&](const std::byte& byte) -> bool { return byte == delim; });
+
+			if ( iterator != bytes.end() )
+				bytes.erase(iterator);
+
+		} while ( iterator != bytes.end() );
 	}
 }
 
@@ -73,11 +158,15 @@ void ToolSet::CreateTmpDirectory()
 
 	if (fs::exists("tmp"))
 	{
+#if 0
 		LOG_TRACE("tmp directory actually exist - remove it");
+#endif
 		fs::remove_all("tmp");
 	}
 
+#if 0
 	LOG_TRACE("Creating tmp directory");
+#endif
 	fs::create_directories("tmp");
 }
 
--- src/tools/ToolSet.h
+++ src/tools/ToolSet.h
@@ -10,13 +10,22 @@
 #define _TOOLSET_H_
 
 #include <vector>
+#include <fstream>
 
 class ToolSet
 {
 public:
 	/// @brief Write sorted chunk into file
-	static void WriteChunkIntoFile(const std::vector<int>& sortedChunk,
-		const unsigned int chunkCount);
+	static bool WriteChunkIntoFile(
+		const std::vector<int>& sortedChunk, const unsigned int chunkCount);
+	/// @brief Read chunk from given file
+	static bool ReadChunkFromFile(
+		std::ifstream& file, std::vector<std::byte>& chunk, 
+		std::size_t chunkSize, std::byte delim = std::byte{ 0x20 });
+	/// @brief Convert from bytes to integers
+	static void SplitAndConvert(
+		std::vector<std::byte>& bytes, std::vector<int>& numbers,
+		const std::byte delim = std::byte{ 0x32 });
 	/// @brief Create tmp directory
 	static void CreateTmpDirectory();
 };
