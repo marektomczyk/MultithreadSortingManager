0861a35724b5434be33d8a46b04d05cc4dcd7090
marek.tomczyk <marek.tomczyk@elzab.pl>
2019-12-09 19:39:10 +0100

    Added all possible algorithm includes into main function

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,14 @@
 // Description : Main function
 //============================================================================
 
-#include <iostream>
 #include "sortingManager/SortingManager.h"
-#include "algorithm/QuickSort/QuickSortAlgorithm.h"
 #include "tools/ToolSet.h"
 #include "log/Logger.h"
+
+// Algorithm's
+#include "algorithm/QuickSort/QuickSortAlgorithm.h"
+#include "algorithm/BitonicSort/BitonicSortAlgorithm.h"
+#include "algorithm/MergeSort/MergeSortAlgorithm.h"
 
 /// @brief Main function
 int main(int argc, char* argv[])
@@ -18,10 +21,19 @@ int main(int argc, char* argv[])
 	// Init logger
 	Logger::Init();
 	// Create sorting manager
-	SortingManager sortingManager(std::make_shared<QuickSortAlgorithm>(), "in.bin", 4u);
+	SortingManager sortingManager(std::make_shared<MergeSortAlgorithm>(), "in.bin", 4u);
 	// Run sorting manager
 	sortingManager.Run();
 
+	// Verify sorted file
+	if ( ToolSet::CheckIfIsSortedProperly("output.bin") )
+	{
+		LOG_INFO("File was sorted properly!");
+	}
+	else
+	{
+		LOG_ERROR("File was not sorted properly!");
+	}
 	return 0;
 }
 
